6c907355e893cd835d01ef82bdf0a8e8c489676c
OGSegu <seguisreal@gmail.com>
2020-09-27 23:34:01 +0300

    Task 5 has been done

--- src/main/java/ru/mail/polis/ads/tasks1/Task5.java
+++ src/main/java/ru/mail/polis/ads/tasks1/Task5.java
@@ -5,10 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.EmptyStackException;
 
-public class Task4 {
+public class Task5 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> queue = new Queue<>();
         String ans;
         String line;
         do {
@@ -16,16 +17,16 @@ public class Task4 {
             try {
                 switch (line) {
                     case "pop":
-                        ans = String.valueOf(stack.pop());
+                        ans = String.valueOf(queue.pop());
                         break;
-                    case "back":
-                        ans = String.valueOf(stack.back());
+                    case "front":
+                        ans = String.valueOf(queue.front());
                         break;
                     case "size":
-                        ans = String.valueOf(stack.size());
+                        ans = String.valueOf(queue.size());
                         break;
                     case "clear":
-                        stack.clear();
+                        queue.clear();
                         ans = "ok";
                         break;
                     case "exit":
@@ -33,7 +34,7 @@ public class Task4 {
                         break;
                     default:
                         int element = Integer.parseInt(line.split(" ")[1]);
-                        stack.push(element);
+                        queue.push(element);
                         ans = "ok";
                         break;
                 }
@@ -45,26 +46,41 @@ public class Task4 {
     }
 }
 
-class Stack<T> {
+class Queue<T> {
+
     private int size = 0;
+    private Node<T> first = null;
     private Node<T> last = null;
 
-    public Stack() {
+    public Queue() {
     }
 
     public void push(T element) {
-        Node<T> cLast = this.last;
-        this.last = new Node<>(element, cLast);
+        Node<T> node;
+        if (size == 0) {
+            node = new Node<>(null, element, null);
+            this.first = node;
+        } else {
+            node = new Node<>(null, element, last);
+            this.last.previous = node;
+        }
+        this.last = node;
         this.size++;
     }
 
     public T pop() {
         if (size == 0)
-            throw new EmptyStackException();
-        T element = last.current;
-        this.last = this.last.next;
+            throw new EmptyQueueException();
+        T element = first.current;
+        this.first = first.previous;
         this.size--;
         return element;
+    }
+
+    public T front() {
+        if (size == 0)
+            throw new EmptyQueueException();
+        return first.current;
     }
 
     public void clear() {
@@ -82,23 +98,27 @@ class Stack<T> {
         return size;
     }
 
-    public T back() {
-        if (size == 0)
-            throw new EmptyStackException();
-        return this.last.current;
+    public Node<T> first() {
+        return first;
     }
 
     public Node<T> last() {
-        return this.last;
+        return  last;
     }
 
     static class Node<T> {
+        Node<T> previous;
         T current;
         Node<T> next;
 
-        public Node(T currentElement, Node<T> nextElement) {
+        public Node(Node<T> previousElement, T currentElement, Node<T> nextElement) {
+            this.previous = previousElement;
             this.current = currentElement;
             this.next = nextElement;
         }
     }
+}
+
+class EmptyQueueException extends RuntimeException {
+
 }
